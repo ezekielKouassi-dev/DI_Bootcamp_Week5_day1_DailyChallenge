1e4805359d70ceceba34d988ca9887639c197991
ezekielKouassi-dev <ezekielkouassi836@yahoo.com>
2023-01-18 16:12:06 +0000

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'httpCall',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
